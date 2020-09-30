8ba1f3835118315344d270480eb7cf5d4e70134b
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-09-29 20:12:36 -0700

    fix(auth): remove  error about user finder

--- functions/src/contexts/api/government/domain/ApiKey.ts
+++ functions/src/contexts/api/government/domain/ApiKey.ts
@@ -2,10 +2,7 @@ import admin from 'firebase-admin';
 
 export async function ensureIsValidApiKey(apiKey: string) {
   if (!apiKey) {
-    throw new Error(`invalid API Key. You need to sign up on https://sanchezcarlosjr.com`);
+    throw new Error(`invalid API Key`);
   }
-  await admin
-    .auth()
-    .getUser(apiKey)
-    .catch(() => `Api key '${apiKey}' not found. You need to sign up on https://sanchezcarlosjr.com`);
+  await admin.auth().getUser(apiKey);
 }
