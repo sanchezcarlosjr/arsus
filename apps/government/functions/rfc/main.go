7d55bc64539e342d2bcc27e0d226428a215bb7c5
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-09-30 19:40:18 -0700

    feat(government): add ensure rfc

--- apps/government/functions/rfc/main.go
+++ apps/government/functions/rfc/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"regexp"
 
 	firebase "firebase.google.com/go"
 	"github.com/aws/aws-lambda-go/events"
@@ -40,6 +41,13 @@ func EnsureAPIKey(apiKey string) string {
 	return ""
 }
 
+func ensureRFC(rfc string) bool {
+	naturalPerson, _ := regexp.MatchString("^(([A-ZÑ&]{4})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$", 
+	rfc)
+	legalPerson, _ := regexp.MatchString("^(([A-ZÑ&]{3})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|(([A-ZÑ&]{3})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|(([A-ZÑ&]{3})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|(([A-ZÑ&]{3})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$", 
+	rfc)
+    return !naturalPerson && !legalPerson
+} 
 
 func response(statusCode int, message map[string]interface{}) (Response, error) {
 	var buf bytes.Buffer
@@ -50,7 +58,7 @@ func response(statusCode int, message map[string]interface{}) (Response, error) 
 	json.HTMLEscape(&buf, body)
 
 	resp := Response{
-		StatusCode:      200,
+		StatusCode:      statusCode,
 		IsBase64Encoded: false,
 		Body:            buf.String(),
 		Headers: map[string]string{
@@ -64,16 +72,22 @@ func response(statusCode int, message map[string]interface{}) (Response, error) 
 func Handler(request events.APIGatewayProxyRequest) (Response, error) {
 	err := EnsureAPIKey(request.QueryStringParameters["apiKey"])
 	if err != "" {
+		return response(401, map[string]interface{}{
+  			"error": err,
+		});
+	}
+	if (ensureRFC(request.QueryStringParameters["rfc"])) {
 		return response(400, map[string]interface{}{
-  			"err": err,
+  			"error": "bad RFC format",
 		});
 	}
 	return response(200, map[string]interface{}{
-		  "isValidRFC": true,
-		  "isLRFC": true,
+		  "isBlockList": false,
+		  "isLRFC": false,
 	});
 }
 
 func main() {
+	InitAdmin()
 	lambda.Start(Handler)
 }
