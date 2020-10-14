428857342645e185a8010c7bb66dbb07cf5f9e85
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-10-14 15:23:41 -0700

    fix(rss): add ionic module

--- src/app/rss/rss.module.ts
+++ src/app/rss/rss.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { RssRoutingModule } from './rss-routing.module';
 import { RssComponent } from './rss.component';
+
 
 
 @NgModule({
   declarations: [RssComponent],
   imports: [
     CommonModule,
+    IonicModule,
     RssRoutingModule
   ]
 })
