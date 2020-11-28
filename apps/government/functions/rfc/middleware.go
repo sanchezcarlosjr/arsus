d42d69d60e95dcc10f2fcbdad0dab3fec0f396ae
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-11-27 20:16:37 -0800

    fix(government): error no-cors

--- apps/government/functions/rfc/middleware.go
+++ apps/government/functions/rfc/middleware.go
@@ -22,22 +22,22 @@ func JSON(statusCode int, message interface{}) (events.APIGatewayProxyResponse, 
 		IsBase64Encoded: false,
 		Body:            buf.String(),
 		Headers: map[string]string{
-			"Content-Type": "application/json",
+			"Content-Type":                 "application/json",
+			"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "GET",
 		},
 	}
 	return resp, nil
 }
 
-
-
 // Middleware is a generic JSON Lambda handler used to chain middleware.
 type Middleware func(events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
 
-
-// MiddlewareAuth ensures api key 
+// MiddlewareAuth ensures api key
 func MiddlewareAuth(next Middleware) Middleware {
-   return Middleware(func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	   	err := EnsureAPIKey(request.QueryStringParameters["apiKey"])
+	return Middleware(func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		err := EnsureAPIKey(request.QueryStringParameters["apiKey"])
 		if err != "" {
 			return JSON(http.StatusUnauthorized, map[string]interface{}{
 				"error": err,
@@ -46,7 +46,6 @@ func MiddlewareAuth(next Middleware) Middleware {
 		return next(request)
 	})
 }
-
 
 func ensureRFC(rfc string) bool {
 	naturalPerson, _ := regexp.MatchString("^(([A-ZÑ&]{4})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$",
@@ -58,12 +57,12 @@ func ensureRFC(rfc string) bool {
 
 // MiddlewareForNoRequest validate query
 func MiddlewareForNoRequest(next Middleware) Middleware {
-   return Middleware(func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	    if ensureRFC(request.QueryStringParameters["rfc"]) {
+	return Middleware(func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		if ensureRFC(request.QueryStringParameters["rfc"]) {
 			return JSON(400, map[string]interface{}{
 				"error": "bad RFC format",
 			})
-	    }
+		}
 		return next(request)
 	})
 }
