5dddeaf8cd158f23ce63ed673ed08572e669c35f
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-11-28 19:16:11 -0800

    feat(government): request to webhook

--- src/app/auth/oauth/oauth.component.ts
+++ src/app/auth/oauth/oauth.component.ts
@@ -1,5 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { LoadingController } from '@ionic/angular';
 import { Store } from '@ngxs/store';
 import { LoginAction } from '@store/auth/auth.actions';
@@ -11,14 +12,29 @@ import { LoginAction } from '@store/auth/auth.actions';
 })
 export class OauthComponent implements OnInit {
   constructor(private store: Store, private loadingController: LoadingController,
-    @Inject(DOCUMENT) private document: Document) { }
+    @Inject(DOCUMENT) private document: Document, private firestore: AngularFirestore) { }
 
   async ngOnInit() {
-    localStorage.setItem('authURLAfterLogin', '/permissions');
-    let params = (new URL(this.document.location.href)).searchParams;
-    localStorage.setItem('oauth', params.toString());
     const loading = await this.loadingController.create();
     await loading.present();
+    localStorage.setItem('authURLAfterLogin', '/permissions');
+    let params = (new URL(this.document.location.href)).searchParams;
+    if (!params.get('apiKey')) {
+      history.go(-1);
+    }
+    this.firestore.collection('users').doc(params.get('apiKey')).get()
+      .subscribe((doc) => {
+        const user = doc.data();
+        if (!doc.exists &&
+          !user.authorizedJavaScriptOrigins.match(params.get('origin')) &&
+          !user.authorizedRedirectURIs.match(params.get('redirect_uri')) &&
+          !user.authorizedWebhooks.match(params.get('webhook'))
+        ) {
+          history.go(-1);
+        }
+      });
+    localStorage.setItem('redirect_uri', params.get('redirect_uri'));
+    localStorage.setItem('webhook', params.get('webhook'));
     this.store
       .dispatch(new LoginAction('google'))
       .toPromise()
--- src/app/auth/permissions/permissions.component.ts
+++ src/app/auth/permissions/permissions.component.ts
@@ -16,6 +16,7 @@ import { ToastService } from './../../@shared/toast.service';
 export class PermissionsComponent implements OnInit {
   loginForm!: FormGroup;
   @ViewChild('slides') slides: IonSlides;
+  private responseAcc: any = null;
   slideOpts = {
     initialSlide: 0,
     allowTouchMove: false
@@ -39,11 +40,23 @@ export class PermissionsComponent implements OnInit {
       filter((uid) => !!uid),
       switchMap((uid) =>
         this.firestore.collection('users').doc(uid).get().pipe(
-          map((doc) => !!doc.data().CURP),
+          map((doc) => doc.data().CURP),
+          switchMap((curp: string) => {
+            if (curp) {
+              return this.http.get('https://us-west4-arsus-production.cloudfunctions.net/curp', {
+                params: {
+                  apiKey: uid,
+                  curp: curp
+                }
+              })
+            }
+            return null;
+          })
         )
       )
     ).subscribe((response) => {
       if (response) {
+        this.responseAcc = response;
         this.slides.slideNext();
       }
       loading.dismiss();
@@ -75,6 +88,7 @@ export class PermissionsComponent implements OnInit {
       )
     ).subscribe((response: any) => {
       loading.dismiss();
+      this.responseAcc = response;
       this.firestore.collection('users').doc(response.uid).update({
         CURP: response.curp
       });
@@ -85,7 +99,11 @@ export class PermissionsComponent implements OnInit {
     });
   }
 
-  login() { }
+  login() {
+    this.http.post(localStorage.getItem('webhook'), {
+      body: this.responseAcc
+    }).toPromise().then(() => document.location.href = localStorage.getItem('redirect_uri'));
+  }
 
   ngOnDestroy() { }
 
@@ -93,6 +111,7 @@ export class PermissionsComponent implements OnInit {
     this.loginForm = this.formBuilder.group({
       permissions: this.formBuilder.group({
         identifyByCURP: [true],
+        shareDataOfCURP: [true],
       }),
       curp: ['', [Validators.required, Validators.pattern(/^[A-Z]{1}[AEIXOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/)]],
     });
