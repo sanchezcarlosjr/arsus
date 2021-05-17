7437c26716c1786b45eabacfbbbf2917b12ea636
SanchezCarlosJr <hola@sanchezcarlosjr.com>
2021-05-16 18:03:35 -0700

    feat(cross_app): show title by app

--- cross_app/lib/views/apps/apps_page_widget.dart
+++ cross_app/lib/views/apps/apps_page_widget.dart
@@ -77,27 +77,51 @@ class AppsInformation extends StatelessWidget {
         }
 
         return new GridView(
-          padding: EdgeInsets.symmetric(horizontal: 30),
+          padding: EdgeInsets.symmetric(horizontal: 10),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 3,
-            crossAxisSpacing: 50,
-            mainAxisSpacing: 50,
-            childAspectRatio: 1,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 25,
+            childAspectRatio: 0.7,
           ),
+          scrollDirection: Axis.vertical,
           children: snapshot.data.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data = document.data();
-            return InkWell(
-                onTap: () async {
-                  print("B");
-                },
-                child: SvgPicture.network(
-                  data["img"],
-                  semanticsLabel: data["title"],
-                  placeholderBuilder: (BuildContext context) => Container(
-                      padding: const EdgeInsets.all(30.0),
-                      child: const CircularProgressIndicator()),
-                  fit: BoxFit.fill,
-                )
+            return  InkWell(
+              onTap: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AppsPageWidget(),
+                  ),
+                );
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  SvgPicture.network(
+                    data["img"],
+                    semanticsLabel: data["title"],
+                    width: 70,
+                    height: 70,
+                    fit: BoxFit.cover,
+                    placeholderBuilder: (BuildContext context) => Container(
+                        padding: const EdgeInsets.all(30.0),
+                        child: const CircularProgressIndicator()
+                    )
+                  ),
+                  Text(
+                    data["title"],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: false,
+                    style: FlutterFlowTheme.bodyText1.override(
+                      fontFamily: 'IBM Plex Sans',
+                      fontSize: 7
+                    ),
+                  )
+                ],
+              ),
             );
           }).toList(),
         );
