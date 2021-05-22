8396366489906706facc1f19cf5c7cff22c71119
SanchezCarlosJr <hola@sanchezcarlosjr.com>
2021-05-22 15:06:31 -0700

    feat(): load data

--- scully.sanchezcarlosjr.config.ts
+++ scully.sanchezcarlosjr.config.ts
@@ -1,8 +1,7 @@
 import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "sanchezcarlosjr",
+  projectRoot: './src',
+  projectName: 'sanchezcarlosjr',
   outDir: './dist/static',
-  routes: {
-  }
+  routes: {},
 };
