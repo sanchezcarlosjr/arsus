8527aa66eb528d5a1e29796780213987d904d993
SanchezCarlosJr <hola@sanchezcarlosjr.com>
2021-05-30 12:32:23 -0700

    refactor(cross_app ine page): from list view to list view builder

--- cross_app/lib/views/pages/home_page/news_component.dart
+++ cross_app/lib/views/pages/home_page/news_component.dart
@@ -2,7 +2,21 @@ import 'package:arsus/views/pages/home_page/new_card_component.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
 
-class NewsComponent extends StatelessWidget {
+class NewsComponent extends StatefulWidget {
+  @override
+  _NewsComponent createState() => _NewsComponent();
+}
+
+class _NewsComponent extends State<NewsComponent> {
+  ScrollController _listScrollController;
+
+  @override
+  void initState() {
+    _listScrollController = ScrollController();
+    _listScrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -14,10 +28,19 @@ class NewsComponent extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Text("Loading...");
         }
-        return new ListView(
-          children: snapshot.data.docs.map((DocumentSnapshot document) => NewCardWidget(data: document.data())).toList(),
+        return ListView.builder(
+          controller: _listScrollController,
+          itemCount: snapshot.data.docs.length,
+          itemBuilder: (context, index) => NewCardWidget(data: snapshot.data.docs[index].data())
         );
       },
     );
+  }
+
+  _scrollListener() {
+    if (_listScrollController.offset >= _listScrollController.position.maxScrollExtent &&
+        !_listScrollController.position.outOfRange) {
+      print("Bottom");
+    }
   }
 }
