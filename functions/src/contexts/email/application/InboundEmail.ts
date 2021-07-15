772d0c5223361dae2297f3451b84c425211457ef
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2021-07-15 08:23:20 -0700

    refactor(email reader): parse inbound email from static method

--- functions/src/contexts/email/application/InboundEmail.ts
+++ functions/src/contexts/email/application/InboundEmail.ts
@@ -3,6 +3,11 @@ import { EmailFrom } from '../domain/EmailFrom';
 import { EmailText } from '../domain/EmailText';
 import { EmailBody } from '../domain/EmailBody';
 import { warn } from '../../shared/Error';
+import * as express from 'express';
+import { FirestoreTypeCommunicationFinderRepository } from '../infrastructure/FirestoreTypeCommunicationFinderRepository';
+import { CloudFunctionsIncomeEmailStrategyMaker } from '../infrastructure/CloudFunctionsIncomeEmailStrategyMaker';
+
+const formidable = require('formidable-serverless');
 
 export class InboundEmail {
   private emailData: Email = {
@@ -19,6 +24,31 @@ export class InboundEmail {
     this.emailData.subject = subject;
     this.emailData.body = body.value;
     this.emailData.to = to;
+  }
+
+  static async parse(request: express.Request): Promise<InboundEmail> {
+    return new Promise((resolve, reject) => {
+      const form = new formidable.IncomingForm();
+      form.parse(request, (errors: any, fields: any) => {
+        if (errors !== null) {
+          reject(errors);
+          return;
+        }
+        resolve(
+          new InboundEmail(
+            new EmailFrom(
+              fields.from,
+              new FirestoreTypeCommunicationFinderRepository(),
+              new CloudFunctionsIncomeEmailStrategyMaker()
+            ),
+            fields.subject,
+            new EmailText(fields.text, fields.subject),
+            new EmailBody(fields.html),
+            fields.to
+          )
+        );
+      });
+    });
   }
 
   async doStrategy() {
--- functions/src/contexts/email/infrastructure/EmailPostWebHook.ts
+++ functions/src/contexts/email/infrastructure/EmailPostWebHook.ts
@@ -1,47 +1,15 @@
 import * as express from 'express';
-import { InboundEmail } from '../application/InboundEmail';
-import { EmailFrom } from '../domain/EmailFrom';
-import { EmailText } from '../domain/EmailText';
-import { FirestoreTypeCommunicationFinderRepository } from './FirestoreTypeCommunicationFinderRepository';
-import { CloudFunctionsIncomeEmailStrategyMaker } from './CloudFunctionsIncomeEmailStrategyMaker';
-import { EmailBody } from '../domain/EmailBody';
 import { warn } from '../../shared/Error';
-
-const formidable = require('formidable-serverless');
+import { InboundEmail } from '../application/InboundEmail';
 
 export class EmailPostWebHook {
   async run(request: express.Request, res: express.Response) {
     try {
-      const incomeEmail = await this.parse(request);
+      const incomeEmail = await InboundEmail.parse(request);
       await incomeEmail.doStrategy();
     } catch (e) {
       warn('EmailPostWebHook', 21, e.message, 'Income Email Parsing');
     }
     return res.sendStatus(200);
-  }
-
-  private parse(request: express.Request): Promise<InboundEmail> {
-    return new Promise((resolve, reject) => {
-      const form = new formidable.IncomingForm();
-      form.parse(request, (errors: any, fields: any) => {
-        if (errors !== null) {
-          reject(errors);
-          return;
-        }
-        resolve(
-          new InboundEmail(
-            new EmailFrom(
-              fields.from,
-              new FirestoreTypeCommunicationFinderRepository(),
-              new CloudFunctionsIncomeEmailStrategyMaker()
-            ),
-            fields.subject,
-            new EmailText(fields.text, fields.subject),
-            new EmailBody(fields.html),
-            fields.to
-          )
-        );
-      });
-    });
   }
 }
